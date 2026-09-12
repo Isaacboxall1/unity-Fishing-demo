43502ec8a29dd0e6782074ab44782fb20c267c18
Isaac <isaacboxall1@gmail.com>
2026-09-12 19:15:42 +0100

    feat: drawing fishing line dynamically

---
+++ Assets/Scripts/FishingLineController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class FishingLineController : MonoBehaviour
+{
+    /** Component References **/
+
+    [SerializeField]
+    private Transform rodTip;
+
+    [SerializeField]
+    private Transform fishingPoint;
+
+    [SerializeField]
+    private float sagAmount = 0.5f;
+
+    /** Member Variables **/
+
+    private LineRenderer lineRenderer;
+
+    /** Lifecycle Functions **/
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    /** Private Helpers **/
+
+    private void UpdateLine()
+    {
+        lineRenderer.SetPosition(0, rodTip.position);
+        lineRenderer.SetPosition(1, CalculateBendPoint());
+        lineRenderer.SetPosition(2, fishingPoint.position);
+    }
+
+    private Vector2 CalculateBendPoint()
+    {
+        Vector2 midPoint = Vector2.Lerp(rodTip.position, fishingPoint.position, 0.5f);
+        midPoint += Vector2.down * sagAmount;
+        return midPoint;
+    }
+}
